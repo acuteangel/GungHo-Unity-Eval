9bda52988b8582ed61336ec1b4aa3638ca0c7d13
acuteangel <41947993+acuteangel@users.noreply.github.com>
2019-03-16 00:28:06 -0700

    added CloudManager Editor

---
+++ New Unity Project/Assets/Scripts/Editor/CloudManagerEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+
+[CustomEditor (typeof (CloudManager))]
+public class CloudManagerEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        var manager = target as CloudManager;
+        var transform = manager.transform;
+        manager.bottomLeft = transform.InverseTransformPoint(
+            Handles.PositionHandle(
+                transform.TransformPoint(manager.bottomLeft),
+                transform.rotation));
+        manager.topRight = transform.InverseTransformPoint(
+            Handles.PositionHandle(
+                transform.TransformPoint(manager.topRight),
+                transform.rotation));
+        Handles.BeginGUI();
+    }
+}
